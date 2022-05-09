fd755092fdd9c668f7d6c26a3ab72ad900cc652e
RKishore1999 <kish240399@gmail.com>
2022-05-09 19:08:49 +0530

    1st update

---
+++ src/org/testing/Anupriya.java
@@ -0,0 +1,9 @@
+package org.testing;
+
+public interface Anupriya {
+	
+	void deeposit();
+	void credit();
+	void savings();
+
+}
---
+++ src/org/testing/Kishore.java
@@ -0,0 +1,35 @@
+package org.testing;
+
+public class Kishore implements Anupriya{
+
+	@Override
+	public void deeposit() {
+		System.out.println("59884");
+		
+	}
+
+	@Override
+	public void credit() {
+		System.out.println("243555");
+	}
+
+	@Override
+	public void savings() {
+		System.out.println("36553");
+		
+	}
+   
+	private void fixed() {
+		System.out.println("5244452");
+
+	}
+	public static void main(String[] args) {
+		Kishore k =new Kishore();
+		k.deeposit();
+		k.credit();
+		k.savings();
+		k.fixed();
+		
+	}
+
+}
